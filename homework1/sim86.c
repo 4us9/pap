b588651b45fe5aa3a0fb8e3c14a5ed09f4b1d94b
Pang <austinpang19@gmail.com>
2026-01-16 06:24:06 -0600

    Edited c program to read binary file. Upon running using 'gcc asm86.c -o decoder', I realized I need to properly download the file.

--- homework1/sim86.c
+++ homework1/sim86.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <stdint.h> 
 
 int main(void) {
 
-    fopen("single_register_mov", "rb");
+    //reading the binary file
+    FILE *f = fopen("single_register_mov", "rb");
+
+    if (!f)
+    {
+        printf("Error: could not open file. \n");
+        return 1;
+    }
+
+    //imported from library to ensure we have an unsigned 8 bit (1 byte) integer.
+    uint8_t buffer[2];
+    
+    //read 1 byte, 2 times, into buffer
+    fread(buffer, 1, 2, f);
+
+    printf("Byte 1: %x\n", buffer[0]);
+    printf("Byte 2: %x\n", buffer[1]);
+    
+    // ... logic continues ...
+    fclose(f);
+    return 0;
+
+
 }
